00221aa763816fb025c142cadfedda22f24a1477
Michael Elfassy <mike.elfassy@gmail.com>
2023-03-04 16:11:27 -0500

    commit tree db53152fc422d5e74f126010fe26089701f2c02f

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setTimeRemaining(() => timeRemaining - 1)
+    }, 1000)
+
+    return function() {
+      clearTimeout(timeoutId)
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
-    onAnswered(isCorrect);
+    onAnswered(false);
   }
+
+  if (timeRemaining <= 0 ) {
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
+
 
   const { id, prompt, answers, correctIndex } = question;
 
